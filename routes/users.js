edea456b2e6903a4f1dc1e46f87b28227a0d9bd8
Ерлан Айдарбаев <yapon4ik777@gmail.com>
2022-09-27 01:56:12 +0600

    HW 89 #1 Создал роут User

--- routes/users.js
+++ routes/users.js
@@ -1,46 +1,31 @@
-import express from "express";
-import User from "../models/User.js";
-import bcrypt from 'bcrypt';
+import express  from "express";
+import User from '../models/User.js';
+
 
 const router = express.Router();
 
-router.post('/', async(req, res)=> {
+router.post('/', async (req, res) => {
     try {
         const user = new User(req.body);
 
         user.generateToken();
-
         await user.save();
-
-        res.send(user);
-
+        res.send(user)
     } catch (error) {
-
         return res.status(400).send(error);
     }
 });
 
-router.post('/sessions', async (req,res)=>{
+
+router.post('/sessions', async (req, res) => {
     const user = await User.findOne({username: req.body.username});
     if(!user) return res.status(400).send({error: 'User not find'});
-
 
     const isMatch = await user.checkPassword(req.body.password);
     if(!isMatch) return res.status(400).send({error: 'Password is wrong'});
 
-    return res.send({message: 'Username and password correct!', user});
+    return res.send({message: 'Username and password is correct', user})
 });
-
-// router.post('/secret', (req,res)=> {
-//     const token = req.get('Authorization');
-//     if(!token) return res.status(401).send({error: "No token present"});
-
-//     const user = await User.findOne({token});
-
-//     if(!user) return res.status(401).send({error: "Wrong token"});
-
-//     return res.send({message: 'secret word', username: user.username});
-// })
 
 
 export default router;
